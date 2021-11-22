32c7cabcc3d619b0bb351feb63aaf048f8ccffcc
Arno Hautala <arno@fracas.net>
2021-11-21 23:03:50 -0500

    cleanup before writing new files

--- all.sh
+++ all.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
+set -e
+
+find ../data/m{1,2,3}map/svg2/ -regextype egrep -regex '.*\.(svg|json)' -not -name '_*' -delete
+
 ./map2svg.py -d ../data/m1map/svg2 -m ../data/m1.map.xml -i ../marathon-utils/map-extras/M1_ignored_polys.txt
 ./map2svg.py -d ../data/m2map/svg2 -m ../data/m2.map.xml -i ../marathon-utils/map-extras/M2_ignored_polys.txt
 ./map2svg.py -d ../data/m3map/svg2 -m ../data/m3.map.xml -i ../marathon-utils/map-extras/M3_ignored_polys.txt
