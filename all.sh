dfbd3264367b2d4d4d46f38f720a7bc70ef25e77
Arno Hautala <arno@fracas.net>
2021-11-30 22:31:49 -0500

    copy overlays when deploying

--- all.sh
+++ all.sh
@@ -10,3 +10,9 @@ find "$BASE_DIR" -mindepth 2 -regextype egrep -regex '.*\.(svg|json)' -delete
 ./map2svg.py -d "$BASE_DIR"/m1 -m ../data/m1.map.xml -i ../marathon-utils/map-extras/M1_ignored_polys.txt
 ./map2svg.py -d "$BASE_DIR"/m2 -m ../data/m2.map.xml -i ../marathon-utils/map-extras/M2_ignored_polys.txt
 ./map2svg.py -d "$BASE_DIR"/m3 -m ../data/m3.map.xml -i ../marathon-utils/map-extras/M3_ignored_polys.txt
+
+for M in 1 2 3
+do
+    cp m$M.json site/m$M/map.json
+    ln -s ../../m${M}_overlays.json site/m$M/overlays.json
+done
